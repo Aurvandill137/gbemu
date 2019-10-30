3dc6b5956cf75962cfed2ad51fbf4b1a6bd43839
Ole H <cerberus885@gmail.com>
2019-10-30 21:30:53 +0100

    removed .idea folder sicne it doesnt belong here

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "cpu.h"
+#include "mmu.h"
 using namespace std;
 
 //TODO: add graphics
@@ -7,6 +8,11 @@ using namespace std;
 //TODO: add cpu cycle emulation
 
 int main() {
+
+
+    extern uint8_t mem[];
+    
+
     cout << "Hello, World!" << std::endl;
     return 0;
 }
